eb1c5b5885e1f2d42f95fe830752321acd839d63
wiroon.sri@ylnq-dev-innosoftkmutt-net <wiroon.sri@innosoft.kmutt.ac.th>
2024-02-13 11:23:40 +0000

    split script

---
+++ setup-default-zsh.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions \
+    && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+mkdir -p ~/.local/share/fonts \
+    && cd ~/.local/share/fonts \
+    && curl -fLO https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf \
+    && curl -fLO https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf \
+    && curl -fLO https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf \
+    && curl -fLO https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+
+cd ~
+
+echo "" > .zshrc
+echo "" > .p10k.zsh
+
+curl -o .zshrc https://raw.githubusercontent.com/zxsylph/setup-script/main/temp-zshrc
+
+curl -0 .p10k.zsh https://raw.githubusercontent.com/zxsylph/setup-script/main/temp-p10k-zsh
