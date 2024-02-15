3cbce1ca50926571b76ad280c257a8890856ecc2
wiroon.sri@ylnq-dev-innosoftkmutt-net <wiroon.sri@innosoft.kmutt.ac.th>
2024-02-15 15:48:44 +0000

    update setup script

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -4,8 +4,8 @@ sudo apt update
 
 sudo apt install zsh -y
 
-chsh -s $(which zsh)
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
-
+chsh -s $(which zsh)
