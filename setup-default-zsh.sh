0f499073f26ada021f6358622456df965635931d
wiroon.sri@ylnq-dev-innosoftkmutt-net <wiroon.sri@innosoft.kmutt.ac.th>
2024-02-15 15:49:18 +0000

    update script move omz to default zsh

--- setup-default-zsh.sh
+++ setup-default-zsh.sh
@@ -2,6 +2,10 @@
 
 cd ~/
 
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions \
     && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
